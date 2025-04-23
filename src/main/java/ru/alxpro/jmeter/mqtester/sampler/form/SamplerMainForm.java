eeb2d51fc0fdc2c324c10e91d1f27fb72948d763
Alexander Bulgakov <alex-vlz@mail.ru>
2025-04-23 23:34:12 +0300

    Updated SamplerMainForm

--- src/main/java/ru/alxpro/jmeter/mqtester/sampler/form/SamplerMainForm.java
+++ src/main/java/ru/alxpro/jmeter/mqtester/sampler/form/SamplerMainForm.java
@@ -23,15 +23,17 @@ public class SamplerMainForm extends JFrame {
 
   private static final long serialVersionUID = 1L;
 
-  JPanel p_Form;
+  JPanel mainPanel;
   JPanel p_MQ_TYPE;
   JTextArea ta_HEADERS;
   JTextField tf_KEY;
   JTextArea ta_MESSAGE;
-  ButtonGroup bg_MQ_TYPE;
   JScrollPane sp_HEADERS;
   JScrollPane sp_MESSAGE;
   JToolBar tb_MESSAGE;
+  GridBagConstraints gbc_headersPanel;
+  GridBagConstraints gbc_keyPanel;
+  GridBagConstraints gbc_bodyPanel;
 
   private void initComponents() {
     addTbMessageListener();
@@ -43,87 +45,85 @@ public class SamplerMainForm extends JFrame {
   public SamplerMainForm() {
     initComponents();
 
-    p_Form = new JPanel();
-    p_Form.setBorder(
+    mainPanel = new JPanel();
+    mainPanel.setBorder(
         BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null, TitledBorder.DEFAULT_JUSTIFICATION,
             TitledBorder.DEFAULT_POSITION, null, null));
-    p_Form.setLayout(new BoxLayout(p_Form, BoxLayout.X_AXIS));
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
-    final JTabbedPane tabbedPane = new JTabbedPane();
-    p_Form.add(tabbedPane);
-    final JPanel panel1 = new JPanel();
-    panel1.setLayout(new BorderLayout(0, 0));
-    tabbedPane.addTab("MESSAGE", panel1);
-    final JPanel panel2 = new JPanel();
-    panel2.setLayout(new GridBagLayout());
-    panel1.add(panel2, BorderLayout.CENTER);
-    panel2.setBorder(
+    final JTabbedPane mainTabs = new JTabbedPane();
+    mainPanel.add(mainTabs);
+    final JPanel messageTab = new JPanel();
+    messageTab.setLayout(new BorderLayout(0, 0));
+    mainTabs.addTab("MESSAGE", messageTab);
+    final JPanel messagePanel = new JPanel();
+    messagePanel.setLayout(new GridBagLayout());
+    messageTab.add(messagePanel, BorderLayout.CENTER);
+    messagePanel.setBorder(
         BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null, TitledBorder.DEFAULT_JUSTIFICATION,
             TitledBorder.DEFAULT_POSITION, null, null));
-    final JPanel panel3 = new JPanel();
-    panel3.setLayout(new BorderLayout(0, 0));
+    final JPanel headersPanel = new JPanel();
+    headersPanel.setLayout(new BorderLayout(0, 0));
     GridBagConstraints gbc;
-    gbc = new GridBagConstraints();
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    gbc.fill = GridBagConstraints.BOTH;
-    gbc.insets = new Insets(2, 2, 2, 2);
-    panel2.add(panel3, gbc);
-    final JLabel label1 = new JLabel();
-    label1.setText("   HEADERS:   ");
-    panel3.add(label1, BorderLayout.NORTH);
+    gbc_headersPanel = new GridBagConstraints();
+    gbc_headersPanel.gridx = 0;
+    gbc_headersPanel.gridy = 0;
+    gbc_headersPanel.fill = GridBagConstraints.BOTH;
+    gbc_headersPanel.insets = new Insets(2, 2, 2, 2);
+    messagePanel.add(headersPanel, gbc_headersPanel);
+    final JLabel headersLabel = new JLabel();
+    headersLabel.setText("HEADERS:");
+    headersPanel.add(headersLabel, BorderLayout.NORTH);
     sp_HEADERS = new JScrollPane();
-    panel3.add(sp_HEADERS, BorderLayout.CENTER);
+    headersPanel.add(sp_HEADERS, BorderLayout.CENTER);
     ta_HEADERS = new JTextArea();
     ta_HEADERS.setRows(5);
     sp_HEADERS.setViewportView(ta_HEADERS);
-    final JPanel panel4 = new JPanel();
-    panel4.setLayout(new BorderLayout(0, 0));
-    gbc = new GridBagConstraints();
-    gbc.gridx = 0;
-    gbc.gridy = 1;
-    gbc.fill = GridBagConstraints.BOTH;
-    gbc.insets = new Insets(2, 2, 2, 2);
-    panel2.add(panel4, gbc);
-    final JLabel label2 = new JLabel();
-    label2.setText("   KEY:   ");
-    panel4.add(label2, BorderLayout.WEST);
+    final JPanel keyPanel = new JPanel();
+    keyPanel.setLayout(new BorderLayout(0, 0));
+    gbc_keyPanel = new GridBagConstraints();
+    gbc_keyPanel.gridx = 0;
+    gbc_keyPanel.gridy = 1;
+    gbc_keyPanel.fill = GridBagConstraints.BOTH;
+    gbc_keyPanel.insets = new Insets(2, 2, 2, 2);
+    messagePanel.add(keyPanel, gbc_keyPanel);
+    final JLabel keyLabel = new JLabel();
+    keyLabel.setText("KEY:");
+    keyPanel.add(keyLabel, BorderLayout.WEST);
     tf_KEY = new JTextField();
-    panel4.add(tf_KEY, BorderLayout.CENTER);
-    final JPanel panel5 = new JPanel();
-    panel5.setLayout(new BorderLayout(0, 0));
-    gbc = new GridBagConstraints();
-    gbc.gridx = 0;
-    gbc.gridy = 2;
-    gbc.weightx = 5.0;
-    gbc.weighty = 4.0;
-    gbc.fill = GridBagConstraints.BOTH;
-    gbc.insets = new Insets(2, 2, 2, 2);
-    panel2.add(panel5, gbc);
-    final JLabel label3 = new JLabel();
-    label3.setText("   BODY:   ");
-    panel5.add(label3, BorderLayout.NORTH);
-    final JPanel panel6 = new JPanel();
-    panel6.setLayout(new BorderLayout(0, 0));
-    panel5.add(panel6, BorderLayout.CENTER);
+    keyPanel.add(tf_KEY, BorderLayout.CENTER);
+    final JPanel bodyPanel = new JPanel();
+    bodyPanel.setLayout(new BorderLayout(0, 0));
+    gbc_bodyPanel = new GridBagConstraints();
+    gbc_bodyPanel.gridx = 0;
+    gbc_bodyPanel.gridy = 2;
+    gbc_bodyPanel.weightx = 5.0;
+    gbc_bodyPanel.weighty = 4.0;
+    gbc_bodyPanel.fill = GridBagConstraints.BOTH;
+    gbc_bodyPanel.insets = new Insets(2, 2, 2, 2);
+    messagePanel.add(bodyPanel, gbc_bodyPanel);
+    final JLabel bodyLabel = new JLabel();
+    bodyLabel.setText("BODY:");
+    bodyPanel.add(bodyLabel, BorderLayout.NORTH);
+    final JPanel bodyContent = new JPanel();
+    bodyContent.setLayout(new BorderLayout(0, 0));
+    bodyPanel.add(bodyContent, BorderLayout.CENTER);
     tb_MESSAGE = new JToolBar();
     tb_MESSAGE.setFloatable(false);
-    panel6.add(tb_MESSAGE, BorderLayout.NORTH);
+    bodyContent.add(tb_MESSAGE, BorderLayout.NORTH);
     sp_MESSAGE = new JScrollPane();
-    panel6.add(sp_MESSAGE, BorderLayout.CENTER);
+    bodyContent.add(sp_MESSAGE, BorderLayout.CENTER);
     ta_MESSAGE = new JTextArea();
     ta_MESSAGE.setRows(9);
     sp_MESSAGE.setViewportView(ta_MESSAGE);
-    final JPanel panel7 = new JPanel();
-    panel7.setLayout(new BorderLayout(0, 0));
-    tabbedPane.addTab("OPTIONS", panel7);
-    final JPanel panel8 = new JPanel();
-    panel8.setLayout(new BorderLayout(0, 0));
-    panel7.add(panel8, BorderLayout.CENTER);
+    final JPanel optionsTab = new JPanel();
+    optionsTab.setLayout(new BorderLayout(0, 0));
+    mainTabs.addTab("OPTIONS", optionsTab);
+    final JPanel optionsPanel = new JPanel();
+    optionsPanel.setLayout(new BorderLayout(0, 0));
+    optionsTab.add(optionsPanel, BorderLayout.CENTER);
     p_MQ_TYPE = new JPanel();
     p_MQ_TYPE.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
-    panel8.add(p_MQ_TYPE, BorderLayout.NORTH);
-
-    bg_MQ_TYPE = new ButtonGroup();
+    optionsPanel.add(p_MQ_TYPE, BorderLayout.NORTH);
   }
 }
