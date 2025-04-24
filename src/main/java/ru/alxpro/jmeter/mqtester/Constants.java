906d3f2b120d634b928170cef1c650269fea758a
Alexander Bulgakov <alex-vlz@mail.ru>
2025-04-24 22:59:44 +0300

    Added form to SamplerGui

--- src/main/java/ru/alxpro/jmeter/mqtester/Constants.java
+++ src/main/java/ru/alxpro/jmeter/mqtester/Constants.java
@@ -2,6 +2,8 @@ package ru.alxpro.jmeter.mqtester;
 
 public class Constants {
 
+  public static final String NAME = "MQ Tester";
+
   private Constants() {
   }
 }
--- src/main/java/ru/alxpro/jmeter/mqtester/sampler/SamplerGui.java
+++ src/main/java/ru/alxpro/jmeter/mqtester/sampler/SamplerGui.java
@@ -1,18 +1,58 @@
 package ru.alxpro.jmeter.mqtester.sampler;
 
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.NORTH;
+import static ru.alxpro.jmeter.mqtester.Constants.NAME;
+
+import java.awt.BorderLayout;
 import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
 
 public class SamplerGui extends AbstractSamplerGui {
 
+  private static boolean initialized;
+
+  private final SamplerMainForm form;
+
+  public SamplerGui() {
+    form = new SamplerMainForm();
+  }
+
+  @Override
+  public String getStaticLabel() {
+    return NAME;
+  }
+
   @Override
   public String getLabelResource() {
-    return "";
+    return getStaticLabel();
+  }
+
+  @Override
+  public void clearGui() {
+    super.clearGui();
+
+    if (!initialized) {
+      initialized = true;
+
+      setLayout(new BorderLayout(5, 5));
+      setBorder(makeBorder());
+      add(makeTitlePanel(), NORTH);
+      add(form.mainPanel, CENTER);
+    }
+  }
+
+  @Override
+  public void configure(TestElement te) {
+    super.configure(te);
   }
 
   @Override
   public TestElement createTestElement() {
-    return null;
+    Sampler sampler = new Sampler();
+    modifyTestElement(sampler);
+
+    return sampler;
   }
 
   @Override
