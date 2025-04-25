02d1099f11b23c7a677595eb6c71f1d4e13d5fa8
Alexander Bulgakov <alex-vlz@mail.ru>
2025-04-25 17:42:14 +0300

    Added logging and other functions to Sampler

--- src/main/java/ru/alxpro/jmeter/mqtester/sampler/Sampler.java
+++ src/main/java/ru/alxpro/jmeter/mqtester/sampler/Sampler.java
@@ -5,32 +5,80 @@ import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.Interruptible;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testelement.TestStateListener;
+import org.apache.jmeter.threads.AbstractThreadGroup;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+import ru.alxpro.jmeter.mqtester.Constants;
+import ru.alxpro.jmeter.mqtester.Helper;
 
 public class Sampler extends AbstractSampler implements TestStateListener, Interruptible {
 
-  @Override
-  public boolean interrupt() {
-    return false;
+  public static final Logger log = LoggingManager.getLoggerForClass();
+
+  private boolean initialized;
+
+  private String whoAmI() {
+    return String.format("%s: %s (%s)", Helper.whoIs(this), Constants.NAME, getName());
+  }
+
+  private void initialize(AbstractThreadGroup threadGroup) {
+    log.info(String.format(whoAmI() + ": Initialize"));
+  }
+
+  private void doAction(SampleResult result) {
+  }
+
+  public Sampler() {
+    log.debug(String.format("%s: %s()", Helper.whoIs(this), NAME));
   }
 
   @Override
   public SampleResult sample(Entry entry) {
-    return null;
+    JMeterContext ctx = JMeterContextService.getContext();
+    JMeterVariables vars = ctx.getVariables();
+
+    SampleResult result = new SampleResult();
+    result.setSampleLabel(getName());
+    result.sampleStart();
+
+    if (!initialized) {
+      initialized = true;
+      initialize(ctx.getThreadGroup());
+    }
+
+    doAction(result);
+
+    result.sampleEnd();
+    return result;
+  }
+
+  @Override
+  public void testStarted(String host) {
+    log.info(String.format(whoAmI() + ": Started (%s)", host));
   }
 
   @Override
   public void testStarted() {
+    testStarted(null);
   }
 
   @Override
-  public void testStarted(String s) {
+  public void testEnded(String host) {
+    log.info(String.format(whoAmI() + ": Ended (%s)", host));
   }
 
   @Override
   public void testEnded() {
+    testEnded(null);
   }
 
   @Override
-  public void testEnded(String s) {
+  public boolean interrupt() {
+    testEnded(null);
+
+    return true;
   }
 }
